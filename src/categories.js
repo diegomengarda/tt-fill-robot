c06f40cb712565ad7ab9d8c262a69a0d2714c974
Diego Mengarda <diegormengarda@gmail.com>
2021-10-15 16:50:33 -0300

    add new categories

--- src/categories.js
+++ src/categories.js
@@ -3,11 +3,13 @@ export const category = {
   IDLE: "21",
   MEETING_CLIENT: "23",
   MEETING_INTERNAL: "24",
+  ABSENCE: "17",
 }
 
 export const taskDescription = {
   FEATURE_DEVELOPMENT: "783",
   DAILY_MEETING: "909",
   NO_ASSIGNED_TASKS: "880",
-  OTHER_MEETINGS: "1013"
+  OTHER_MEETINGS: "1013",
+  NATIONAL_HOLIDAY: "749",
 }
